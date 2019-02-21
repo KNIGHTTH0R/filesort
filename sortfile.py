c1f657ae5b6ecf98aa1caa316b0b6d327fb0f6d7
cubefreaker <cuber_ae@yahoo.com>
2019-02-21 22:56:20 +0700

    add script for moving entire file in path

--- sortfile.py
+++ sortfile.py
@@ -3,28 +3,48 @@ import shutil
 
 def makedir():
     '''create folder'''
+    global path
     path = os.getcwd()
     directory = os.path.join(path, '{}' .format(dirname))
     
     if not os.path.exists(directory):
-        return os.makedirs(directory)
+        os.makedirs(directory)
+        return searchfile()
     else:
         print('\nfolder already exist!')
         return main()
 
 def searchfile():
     '''search specific file'''
-    return
+    global filename
+
+    filename = []
+    olpath = path + '/' + oldir
+
+    for f in os.listdir(olpath):
+        if os.path.isfile(os.path.join(olpath, f)):
+            filename.append(f)
+
+    return movefile()
 
 def movefile():
     '''move sorted file'''
+
+    for i in filename:
+        dir_from = path + '/' + oldir + '/' + i
+        dir_to = path + '/' + dirname + '/' + i
+        shutil.move(dir_from, dir_to)
+    
     return
 
 def main():
     '''main proccess'''
+    
     global dirname
+    global oldir
+    oldir = input('\nInput source folder: ')
     dirname = input('\nInput new folder name: ')
-    makedir()
-    return
+
+    return makedir()
 
 main()
