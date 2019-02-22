a9c3075618367c5b472173bc98a445c4cbb82136
cubefreaker <cuber_ae@yahoo.com>
2019-02-23 03:09:16 +0700

    changes on search sub dir function

--- sortfile.py
+++ sortfile.py
@@ -5,7 +5,7 @@ def makedir():
     '''create folder'''
     global path
     path = os.getcwd()
-    directory = os.path.join(path, '{}' .format(dirname))
+    directory = os.path.join(path, dirname)
     
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -17,25 +17,34 @@ def makedir():
 def searchfile():
     '''search specific file'''
     global filename
-
-    filename = []
-    olpath = path + '/' + oldir
+    global dirpath
     
-    #filter by filename
-    def findtitle():
-        for f in os.listdir(olpath):
-            if os.path.isfile(os.path.join(olpath, f)):
-                if filepart in f:
-                    filename.append(f)
+    filename = []
+    dirpath = ''
+    #find through entire folder
+    def findsubdir(parent, subdir):
+        
+        newpath = os.path.join(parent, subdir)
+        dirpath = os.path.join(dirpath, subdir)
 
-    #filter by filecontent
-    def findcontent():
-        for f in os.listdir(olpath):
-            with open(f) as file:
-                contents = file.read()
-                if filepart in contents:
-                    filename.append(f)
+        for f in os.listdir(newpath):
+            if findby == '1':
+                if os.path.isfile(os.path.join(newpath, f)):
+                    if filepart in f:
+                        filename.append(f)
+                else:
+                    findsubdir(newpath, f)
+            else:
+                if os.path.isfile(os.path.join(newpath, f)):
+                    if '.txt' in f:
+                        with open(os.path.join(dirpath, f)) as file:
+                            contents = file.read()
+                            if filepart in contents:
+                                filename.append(f)
+                else:
+                    findsubdir(newpath, f)
 
+    findsubdir(path, oldir)
 
     return movefile()
 
@@ -55,9 +64,15 @@ def main():
     global dirname
     global oldir
     global filepart
+    global findby
+    findby = input('\nSelect the method:\n[1] find by file name\n[2] find by file content\n>> ')
+    if findby != ('1' and '2'):
+        print('\nPlease input 1 or 2 only!')
+        return main()
+
     oldir = input('\nInput source folder: ')
-    dirname = input('\nInput new folder name: ')
-    filepart = input('\nInput part of the file name: ')
+    dirname = input('\nInput new folder name to create: ')
+    filepart = input('\nInput keyword: ')
     return makedir()
 
 main()
